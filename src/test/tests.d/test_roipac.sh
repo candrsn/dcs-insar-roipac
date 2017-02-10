bc33eb2b8436438fa68175d615d6d1eb0788d02a
fabricebrito <fabrice.brito@terradue.com>
2017-02-10 16:12:26 +0100

    update

--- src/test/tests.d/test_roipac.sh
+++ src/test/tests.d/test_roipac.sh
@@ -3,14 +3,22 @@
 export ciop_job_include="/usr/lib/ciop/libexec/ciop-functions.sh"
 source ./test_common.sh
 
-test_bash_n()
+test_bash_n_run()
 {
 
-  bash -n ../main/app-resources/aux/run
+  bash -n ../main/app-resources/roipac/run
   res=$?
-  assertEquals "bash -n validation failed" \
+  assertEquals "bash -n validation of roipac/run failed" \
   "0" "${res}"
 }
 
+test_bash_n_lib()
+{
+
+  bash -n ../main/app-resources/roipac/lib/functions.sh
+  res=$?
+  assertEquals "bash -n validation of roipac/lib/functions.sh failed" \
+  "0" "${res}"
+}
 
 . ${SHUNIT2_HOME}/shunit2
